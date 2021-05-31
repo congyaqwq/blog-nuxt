0929bb2a03c77b82b073589fe627d7a63ffc94ec
李聪 <lc182540@163.com>
2021-05-31 12:25:36 +0800

    fix:换色bug

--- layouts/components/my-radio.vue
+++ layouts/components/my-radio.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-radio middle-flex" :class="active" @click="changeTheme">
-    <div :class="['button']" ></div>
-    <div :class="['button']" ></div>
+    <div :class="['button']"></div>
+    <div :class="['button']"></div>
   </div>
 </template>
 
@@ -10,44 +10,44 @@ import { defineComponent, ref } from "@nuxtjs/composition-api";
 
 export default defineComponent({
   setup() {
-    const active = ref('dark')
+    const active = ref("light");
 
     return {
       active,
-    }
+    };
   },
-  methods:{
-    changeTheme(){
-      this.active = this.active==='light'?'dark':'light'
-      this.$colorMode.preference = this.active
+  methods: {
+    changeTheme() {
+      this.active = this.active === "light" ? "dark" : "light";
+      this.$colorMode.preference = this.active;
       // this.$emit('change',active)
-    }
-  }
-})
+    },
+  },
+});
 </script>
 
 <style lang="less" scoped>
 .my-radio {
   position: relative;
-  width:60px;
-  height:20px;
-  border:2px solid #ccc;
+  width: 60px;
+  height: 20px;
+  border: 2px solid #ccc;
   overflow: hidden;
   border-radius: 2px;
   background-color: #fff;
   user-select: none;
-  color:#333;
-  font-size: 12px;;
+  color: #333;
+  font-size: 12px;
   &::after {
     position: absolute;
-    top:0;
-    bottom:0;
+    top: 0;
+    bottom: 0;
     right: 0;
-    content:'';
-    width:30px;
-    height:100%;
+    content: "";
+    width: 30px;
+    height: 100%;
     background-color: #0d2538;
-    transition: transform .3s;
+    transition: transform 0.3s;
   }
   &.light {
     &::after {
@@ -60,10 +60,10 @@ export default defineComponent({
     }
   }
   .button {
-    width:40px;
+    width: 40px;
     height: 100%;
     cursor: pointer;
-    transform:translateX(0px);
+    transform: translateX(0px);
   }
 }
 </style>
