ca6d02e3409552106dff864c58a2766c81122189
李聪 <lc182540@163.com>
2021-09-23 17:27:16 +0800

    feat: readme

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# blog-nuxt
+# 个人博客
 
-## Vue3 语法+nuxt
+## 项目简介:
 
-使用的库是<a href="https://github.com/nuxt-community/composition-api">@nuxtjs/composition-api</a>
-成功将 Vue3 项目更新为支持 SEO 的 nuxt 项目。  
-项目预览：<a href="https://www.congyaqwq.top">https://www.congyaqwq.top</a>
+1. 使用 Vue3 语法  
+    使用的库是<a href="https://github.com/nuxt-community/composition-api">@nuxtjs/composition-api</a>
+   成功将 Vue3 项目更新为支持 SEO 的 nuxt 项目。
+2. 适配移动端
+3. 换肤功能
+4. 性能良好（lighthouse 测试）
+
+项目预览：<a href="https://www.congyaqwq.top">https://www.congyaqwq.top</a>  
+后端在另一个仓库中
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,18 +1,17 @@
 <template>
   <ul>
-    <li>微信:lc18379686811</li>
+    <li>微信:<span>bGMxODM3OTY4NjgxMQ==</span></li>
     <li>
-      <a href="https://github.com/congyaqwq" target="_blank"
-        >github</a
-      >
+      <a href="https://github.com/congyaqwq" target="_blank">github</a>
     </li>
+    <li>打好基础才是最重要</li>
   </ul>
 </template>
 
 <script>
-import { defineComponent } from "@nuxtjs/composition-api";
+import { defineComponent } from '@nuxtjs/composition-api'
 
-export default defineComponent({});
+export default defineComponent({})
 </script>
 
 <style scoped>
